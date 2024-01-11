2785aa9eadcc0a998ef98f060fb0ac6ec327a146
DESKTOP-LDD4AU9\User <bgh2584@gmail.com>
2024-01-11 18:57:41 +0900

    파이썬기초 01

---
+++ 01_money.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+"""
+where = input("경기장은 어디입니까?")
+win = input("이긴 팀은 어디입니까?")
+loss = input("진 팀은 어디입니까?")
+mvp = input("우수선수는 누구입니까?")
+score = input("스코어는 몇대몇입니까?")
+
+print("=============================================")
+print(f"오늘 {where}에서 야구경기가 열렸습니다.")
+print(f"{win}과 {loss}은 치열한 공방전을 펼쳤습니다.")
+print(f"{mvp}의 맹활약으로 {win}가 {loss}를 {score}로 이겼습니다.")
+print("=============================================")
+"""
+
+money = int(input("투입한돈 : "))
+price = int(input("물건값 : "))
+
+change = money - price
+print("거스름돈 : ",change)
+print("500원 동전개수 : ",change//500)
+print("100원 동전개수 : ",(change%500)//100)
---
+++ history.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Jan  8 11:01:32 2024)---
+runcell(0, 'C:/Users/User/.spyder-py3/temp.py')
+
+## ---(Tue Jan  9 09:35:59 2024)---
+runcell(0, 'C:/Users/User/untitled0.py')
+runcell(0, 'C:/Users/User/untitled1.py')
+runcell(0, 'C:/Users/User/untitled2.py')
+runcell(0, 'C:/Users/User/untitled3.py')
+
+## ---(Thu Jan 11 11:26:47 2024)---
+runcell(0, 'C:/Users/User/untitled4.py')
+runfile('C:/Users/User/untitled4.py', wdir='C:/Users/User')
+runcell(0, 'C:/Users/User/untitled4.py')
+runfile('C:/Users/User/untitled5.py', wdir='C:/Users/User')
+runcell(0, 'C:/Users/User/untitled5.py')
+runcell(0, 'C:/Users/User/untitled6.py')
+runcell(0, 'C:/Users/User/untitled7.py')
+runfile('C:/Users/User/untitled8.py', wdir='C:/Users/User')
+runfile('C:/Users/User/.spyder-py3/10_image.py', wdir='C:/Users/User/.spyder-py3')
+runcell(0, 'C:/Users/User/.spyder-py3/10_image.py')
+runfile('C:/Users/User/.spyder-py3/10_image.py', wdir='C:/Users/User/.spyder-py3')
+runfile('C:/Users/User/.spyder-py3/untitled10.py', wdir='C:/Users/User/.spyder-py3')
+runfile('C:/Users/User/.spyder-py3/10_image.py', wdir='C:/Users/User/.spyder-py3')
