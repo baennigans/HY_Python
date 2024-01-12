a66a9420d5afb18efc1425e94bfa393c11af6090
DESKTOP-LDD4AU9\User <bgh2584@gmail.com>
2024-01-12 15:27:04 +0900

    파이썬기초 02

---
+++ 02_num.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 09:42:59 2024
+
+@author: User
+"""
+
+num = int(input("정수를 입력하시오 : "))
+
+if num % 2 == 0 :
+    print("짝수입니다.")
+else :
+    print("홀수입니다.")
