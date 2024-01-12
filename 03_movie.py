6fcd3eb8aa3d0b3691e810657123134de3955a0a
DESKTOP-LDD4AU9\User <bgh2584@gmail.com>
2024-01-13 00:29:41 +0900

    파이썬기초 03

---
+++ 03_movie.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 09:50:03 2024
+
+@author: User
+"""
+
+print("안녕하세요? 폴리텍영화관입니다.")
+age = int(input("나이를 입력하시오 :"))
+
+if age >= 15 : 
+    print("이 영화를 보실 수 있습니다.")
+else :
+    print("이 영화를 보실 수 없습니다.")
